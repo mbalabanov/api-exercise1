5a3998bbf4f80421d9c2611bff351c1c0ae7add0
marinbalabanov <marincomics@gmail.com>
2020-11-23 13:33:00 +0200

    Changed title

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API Ex. 1</title>
+    <title>API Exercises</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top shadow">
-    <a class="navbar-brand" href="#bbcnews">API Exercise 1</a>
+    <a class="navbar-brand" href="#bbcnews">API Exercises</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
